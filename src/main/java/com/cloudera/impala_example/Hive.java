be675045a2e2879acdeb5d76f63d2b42089aba1e
116301 <Kailin.Chen@sinopac.com>
2018-09-06 15:32:20 +0800

    string format

--- src/main/java/com/cloudera/impala_example/Hive.java
+++ src/main/java/com/cloudera/impala_example/Hive.java
@@ -3,13 +3,14 @@ package com.cloudera.impala_example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Hive {
 
-    private static final String SQL_STATEMENT = "select * from master";
-    private static final String IMPALA_HOST = "35.234.23.58";
+    private static final String SQL_STATEMENT = "select * from products";
+    private static final String IMPALA_HOST = "35.239.91.94";
     private static final String IMPALA_PORT = "10000";
-    private static final String CONNECTION_URL = String.format("jdbc:hive2://%s:%s/baseball", IMPALA_HOST, IMPALA_PORT);
+    private static final String CONNECTION_URL = String.format("jdbc:hive2://%s:%s/default", IMPALA_HOST, IMPALA_PORT);
     private static final String USER_NAME = "cloudera";
     private static final String USER_PWD = "cloudera";
 
@@ -20,11 +21,10 @@ public class Hive {
         try {
             Class.forName("org.apache.hive.jdbc.HiveDriver");
             Connection connection = DriverManager.getConnection(CONNECTION_URL, USER_NAME, USER_PWD);
-            ResultSet resultSet = connection.createStatement().executeQuery(SQL_STATEMENT);
-            int i = 1;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL_STATEMENT);
             while (resultSet.next()) {
-                System.out.println(String.format("No.%d : %s %s", i, resultSet.getString(1), resultSet.getString(2)));
-                i++;
+                System.out.println(String.format("No.%04d | $%12f | %s", resultSet.getRow(), resultSet.getDouble(5), resultSet.getString(3)));
             }
         } catch (Exception e) {
             System.out.println(e.toString());
--- src/main/java/com/cloudera/impala_example/Impala.java
+++ src/main/java/com/cloudera/impala_example/Impala.java
@@ -3,11 +3,12 @@ package com.cloudera.impala_example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Impala {
 
     private static final String SQL_STATEMENT = "select * from tokenized_access_logs limit 2000";
-    private static final String IMPALA_HOST = "35.234.23.58";
+    private static final String IMPALA_HOST = "35.239.91.94";
     private static final String IMPALA_PORT = "21050";
     private static final String CONNECTION_URL = String.format("jdbc:hive2://%s:%s/default;auth=noSasl", IMPALA_HOST, IMPALA_PORT);
 
@@ -17,11 +18,15 @@ public class Impala {
         try {
             Class.forName("org.apache.hive.jdbc.HiveDriver");
             Connection connection = DriverManager.getConnection(CONNECTION_URL);
-            ResultSet resultSet = connection.createStatement().executeQuery(SQL_STATEMENT);
-            int i = 1;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL_STATEMENT);
             while (resultSet.next()) {
-                System.out.println(String.format("No.%d : %s %s", i, resultSet.getString(1), resultSet.getString(2)));
-                i++;
+                String s = String.format("No.%04d | %15s | %3s | %s",
+                        resultSet.getRow(),
+                        resultSet.getString(2),
+                        resultSet.getString(6),
+                        resultSet.getString(4));
+                System.out.println(s);
             }
         } catch (Exception e) {
             System.out.println(e.toString());
